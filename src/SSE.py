895769add53d61f9966c7b6f05fee6b7963577fb
Amine <amine75135@gmail.com>
2025-02-11 05:42:21 +0100

    feature client

--- src/SSE.py
+++ src/SSE.py
@@ -1,35 +1,81 @@
-import hashlib
+import os
+import json
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
-class Encryptor:
-    def __init__(self, key):
-        self.key = get_random_bytes(16)
+CLIENTPATH = "./Client/"
+SERVEURPATH = "./Serveur/"
+extentions = (".txt")
 
+class Encryptor:
+    """
+    On pad pour que les données aient une longueur de 16 octets
+    Retourne b'<data>800000' (un multiple de 16 afin de pouvoir utiliser AES)
+    """
     def pad(self, data):
-        # on pad pour que les données aient une longueur de 16 octets (besoin AES)
         padding_length = 16-(len(data)%16)
         return data + b'8' + ('0'*(padding_length-1)).encode()
-        # b'<data>800000' pour que le total soit un multiple de 16 afin de pouvoir utiliser AES
     
-    def unpad(self,data):
+    """
+    On recupere la valeur correspondant au nombre d'octets ajouté et 
+    on enleve ce nombre de characteres à la fin du message
+    """
+    def unpad(self, data):
         return data[:-data[-1]]
-        # on recupere la valeur correspondant au nombre d'octets ajouté et on enleve ce nombre de charactere
 
-    def encrypt_document(self, content):
-        cipher = AES.new(self.key, AES.MODE_CBC) # on chiffre le doc en AES mode CBC
-        iv = cipher.iv
-        encrypted_content = cipher.encrypt(self.pad(content.encode()))
-        return bytes(iv) + encrypted_content
+    # Pas fini
+    def encrypt_documents(self, key, client_path):
+        for document in os.listdir(client_path):
+            if document.endswith(extentions):
+                cipher = AES.new(key, AES.MODE_CBC) #Cypher Block Chaining
+                iv = cipher.iv
+                with open(client_path + document, 'r', encoding='utf-8') as file:
+                    encrypted_content = cipher.encrypt(self.pad(file.read().encode()))
+                    encrypted_doc = client_path + os.path.splitext(document)[0] + '.enc'
+                    with open(encrypted_doc, 'wb') as f:                                           
+                        f.write(bytes(iv) + encrypted_content)                                       
 
-    def decrypt_document(self, encrypted_data):
-        iv = encrypted_data[:16]
-        encrypted_content = encrypted_data[16:]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        decrypted_content = self.unpad(cipher.decrypt(encrypted_content))
+    def decrypt_document(self, key, client_path, encrypted_data):
+        with open(client_path + encrypted_data, 'rb') as f:
+            iv = f.read(16)
+            encrypted_content = f.read()
+            cipher = AES.new(key, AES.MODE_CBC, iv)
+            decrypted_content = self.unpad(cipher.decrypt(encrypted_content))
         return decrypted_content.decode()
 
-    def encrypt_word(self, word):
-        cipher = AES.new(self.key, AES.MODE_ECB) # on veut chiffrer le mot en mode ECB
-        padded_word = self.pad(word.encode()) # on le pad avant pour qu'il fasse 16 octet
+    def encrypt_word(self, key, word):
+        cipher = AES.new(self.key, AES.MODE_ECB)
+        padded_word = self.pad(word.encode())
         return cipher.encrypt(padded_word)
+
+    """
+    On creer l'index en clair du dossier <client_path>
+    """
+    def create_index(self, key, client_path):
+        if not os.path.exists(client_path):
+            raise Exception("Le dossier {client_path} n'existe pas.")
+        index = {}
+        for document in os.listdir(client_path):
+            if document.endswith(extentions):
+                with open(client_path + document, 'r', encoding='utf-8') as file:  
+                    for line in file:                                        
+                        for word in line.split():                                          # ['The', 'question', 'isn', 't,', ...]
+                            clean_word = word.strip(",.?!:;()[]{}\"'\n\t-")                # 'the', 'question', 'isn', 't'
+                            if clean_word not in index:
+                                index[clean_word] = []                                     # {'the': [], 'question': [], 'isn': [], 't': []}
+                            if document not in index[clean_word]: 
+                                index[clean_word].append(document)                         # {'the': ['doc1.txt'], 'question': ['doc1.txt'], ...}
+        # Sauvegarde du dictionnaire en JSON
+        with open(client_path + "/index.json", 'w', encoding='utf-8') as json_file:
+            json.dump(index, json_file, ensure_ascii=True)
+
+    def encrypt_index(self, key, index_path):
+        with open(index_path, 'r', encoding='utf-8') as index_file:
+            index = json.load(index_file)
+        cipher = AES.new(key, AES.MODE_ECB)
+        encrypted_index = {}
+        for word, docs in index.items():
+            encrypted_word = self.encrypt_word(key, word)
+            encrypted_docs = [self.encrypt_word(key, doc) for doc in docs]
+            encrypted_index[encrypted_word] = encrypted_docs
+        return encrypted_index
--- src/SSE_bdd.py
+++ src/SSE_bdd.py
@@ -1,27 +1,36 @@
 import os
 import json
 
+CLIENTPATH = "./Client/"
+SERVEURPATH = "./Serveur/"
+INDEXPATH = "./Serveur/index.json"
+
 class Database:
-    def __init__(self, user_id, encryptor, server_path="Serveur"):
+    def __init__(self, user_id, encryptor):
         self.user_id = user_id
         self.encryptor = encryptor
-        self.server_path = os.path.join(server_path, user_id)  # User specific folder
-        self.index_path = os.path.join(self.server_path, 'index.json')
+        self.server_path, self.index_path = SERVEURPATH, INDEXPATH
         self.index = {}
 
         if not os.path.exists(self.server_path):
             os.makedirs(self.server_path)
-
+            raise Exception("Le dossier Serveur n'existait pas, il a été crée.")
         self.load_index()
 
+    """
+    Save the self.index dict to a file + convert keys to hexa
+    """
     def save_index(self):
         with open(self.index_path, 'w') as file:
-            json.dump({k.hex(): v for k, v in self.index.items()}, file) # save the self.index dict to a file + convert keys to hexa
+            json.dump({k.hex(): v for k, v in self.index.items()}, file)
 
+    """
+    Load index from file (convert hexa to binary)
+    """
     def load_index(self):
         if os.path.exists(self.index_path):
             with open(self.index_path, 'r') as file:
-                self.index = {bytes.fromhex(k): v for k, v in json.load(file).items()} # load index from file (convert hexa to binary)
+                self.index = {bytes.fromhex(k): v for k, v in json.load(file).items()}
 
     def store_encrypted_document(self, doc_id, encrypted_data):
         # Encrypted doc content is stored in the user's directory
@@ -39,6 +48,7 @@ class Database:
 
     def create_index(self, doc_id, words):
         # Encrypt words and associates them with the doc where they are
+        
         for word in words:
             encrypted_word = self.encryptor.encrypt_word(word)
             if encrypted_word not in self.index:
